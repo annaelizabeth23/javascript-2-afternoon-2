8eea82713468ec0d936cad6e8700bf275e274857
Anna Bender <annaelizabeth23@gmail.com>
2018-01-28 11:47:33 -0700

    completed required practice problems

--- practice.js
+++ practice.js
@@ -18,8 +18,11 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first (arr) {
+  return arr[0];
+}
 
-
+//51
 
 ////////// PROBLEM 2 //////////
 
@@ -33,8 +36,11 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last (arr) {
+  return arr.pop();
+}
 
-
+//48
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +55,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper (family) {
+  for (let i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
 
+//45
 
 ////////// PROBLEM 4 //////////
 
@@ -63,8 +75,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper (letters) {
+  for (let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
-
+//42
 
 ////////// PROBLEM 5 //////////
 
@@ -78,7 +95,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder (nums) {
+  var newArr = []; 
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] % 2 == 0) {
+    newArr.push(nums[i]);
+    }
+  }
+  return newArr;
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
